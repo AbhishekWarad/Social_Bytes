bcf042f357815a461175978bf290ddd1207fbfdd
AbhishekWarad <93395625+AbhishekWarad@users.noreply.github.com>
2023-08-28 20:45:42 +0530

    Add files via upload

---
+++ q.11.sql
@@ -0,0 +1,16 @@
+show databases
+use newforme
+
+CREATE TABLE Student (
+    ID INT PRIMARY KEY NOT NULL,
+    Name VARCHAR(20) NOT NULL,
+    Age INT NOT NULL,
+    Address VARCHAR(25)
+);
+
+
+INSERT INTO Student VALUES(1, 'Ae', 20, 'davi nagar ,kalaburgi');
+INSERT INTO Student VALUES(2, 'B', 22, 'santosh nagar ,kalaburgi');
+INSERT INTO Student VALUES(3, 'C', 21, 'ineuron nagar ,kalaburgi');
+INSERT INTO Student VALUES(4, 'Dd', 23, 'Krish nayak,kalaburgi');
+INSERT INTO Student VALUES (5, 'E', 19, 'sunny nagar ,kalaburgi');
